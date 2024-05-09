cc99b956bf283a0ba10033d1d564b1ea14c7d028
Subhash Ch. Pal <34298283+Subhash-Pal@users.noreply.github.com>
2024-05-09 15:32:59 +0530

    Update dump.go

--- dump.go
+++ dump.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "sync"
 )
+
 func main() {
     var wg sync.WaitGroup
     pingCh := make(chan string)
@@ -12,20 +13,21 @@ func main() {
     wg.Add(1) // One WaitGroup for the first goroutine
     go func() {
         defer wg.Done()
+
         for i := 0; i < 26; i++ {
             letter := string(rune('A' + i)) // Convert integer to corresponding letter
-            fmt.Print("", string(letter[0]))
+            fmt.Println("Ping", letter, i+1) // Print the letter and its corresponding iteration count
             pongCh <- letter
             <-pingCh
-          // println("next" , <-pingCh)
         }
     }()
+
     go func() {
         wg.Add(1) // One WaitGroup for the second goroutine
         defer wg.Done()
         for i := 0; i < 26; i++ {
             letter := <-pongCh
-            fmt.Println("", i+1)
+            fmt.Println("Pong", letter, i+1) // Print the letter and its corresponding iteration count
             pingCh <- string(rune(letter[0] + 1)) // Send next letter in sequence
         }
     }()
