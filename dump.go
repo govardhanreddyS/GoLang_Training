73996dc0ffdcc25edf6de168c47936b92b388833
Subhash Ch. Pal <34298283+Subhash-Pal@users.noreply.github.com>
2024-05-09 16:08:17 +0530

    Update dump.go

--- dump.go
+++ dump.go
@@ -1,38 +1,55 @@
 package main
 
 import (
-    "fmt"
-    "sync"
+	"fmt"
+	"time"
 )
 
-func main() {
-    var wg sync.WaitGroup
-    pingCh := make(chan string)
-    pongCh := make(chan string)
-
-    wg.Add(1) // One WaitGroup for the first goroutine
-    go func() {
-        defer wg.Done()
-
-        for i := 0; i < 26; i++ {
-            letter := string(rune('A' + i)) // Convert integer to corresponding letter
-            fmt.Println("Ping", letter, i+1) // Print the letter and its corresponding iteration count
-            pongCh <- letter
-            <-pingCh
-        }
-    }()
-
-    go func() {
-        wg.Add(1) // One WaitGroup for the second goroutine
-        defer wg.Done()
-        for i := 0; i < 26; i++ {
-            letter := <-pongCh
-            fmt.Println("Pong", letter, i+1) // Print the letter and its corresponding iteration count
-            pingCh <- string(rune(letter[0] + 1)) // Send next letter in sequence
-        }
-    }()
-
-    wg.Wait() // Wait for the first goroutine to finish
-    close(pingCh)
-    close(pongCh)
+func goroutineA(ch chan bool, quit chan struct{}) {
+	for {
+		select {
+		case <-ch:
+			fmt.Println("Goroutine A is triggered")
+		case <-quit:
+			fmt.Println("Goroutine A is stopping")
+			return
+		}
+	}
 }
+
+func goroutineB(ch chan bool, quit chan struct{}) {
+	for {
+		select {
+		case <-ch:
+			fmt.Println("Goroutine B is triggered")
+		case <-quit:
+			fmt.Println("Goroutine B is stopping")
+			return
+		}
+	}
+}
+func main() {
+	trigger := make(chan bool)
+	quit := make(chan struct{})
+
+	go goroutineA(trigger, quit)
+	go goroutineB(trigger, quit)
+
+	// Simulating conditions to trigger different goroutines
+	go func() {
+		for {
+			time.Sleep(2 * time.Second)
+			trigger <- true // Trigger goroutine A
+			time.Sleep(3 * time.Second)
+			trigger <- true // Trigger goroutine B
+		}
+	}()
+
+	// Stop the goroutines after a certain period
+	time.Sleep(15 * time.Second)
+	close(quit)
+
+	// Keep the main goroutine alive for a while to observe the output
+	time.Sleep(2 * time.Second)
+}
+
