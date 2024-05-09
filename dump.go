562b6bc438878cbb53e41011f6bf3ce613900d9a
Subhash Ch. Pal <34298283+Subhash-Pal@users.noreply.github.com>
2024-05-09 18:35:21 +0530

    factorial_test.go

--- dump.go
+++ dump.go
@@ -1,36 +1,49 @@
 package main
 
 import (
-    "math/big"
+    //"math"
     "testing"
 )
 
-const size = 10000000 // Size of the slice
-
-var nums []int
-
-func init() {
-    // Initialize the slice with numbers from 1 to size
-    nums = make([]int, size)
-    for i := 0; i < size; i++ {
-        nums[i] = i + 1
-    }
-}
-
-func BenchmarkLoopSum(b *testing.B) {
+func BenchmarkFactorialIterative(b *testing.B) {
     for i := 0; i < b.N; i++ {
-        sum := 0
-        for _, num := range nums {
-            sum += num
-        }
+        factorialIterative(10) // Change the input value as needed
+    }
+} //go test -benchmem -run=^$ -bench ^BenchmarkFactorialIterative$ bench-test
+//go test -benchmem -bench .  
+
+func BenchmarkFactorialRecursive(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        factorialRecursive(10) // Change the input value as needed
     }
 }
 
-func BenchmarkBigSum(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        var sum big.Int
-        for _, num := range nums {
-            sum.Add(&sum, big.NewInt(int64(num)))
-        }
+// Function to calculate factorial iteratively
+func factorialIterative(n int) int {
+    result := 1
+    for i := 1; i <= n; i++ {
+        result *= i
     }
+    return result
 }
+
+// Function to calculate factorial recursively
+func factorialRecursive(n int) int {
+    if n <= 1 {
+        return 1
+    }
+    return n * factorialRecursive(n-1)
+}
+//go test -benchmem -bench .          
+/*
+BenchmarkFactorialIterative-12: This benchmark ran 167881663 iterations of the
+ factorialIterative function. It took an average of 7.221 nanoseconds per operation, 
+ and there were 0 memory allocations (B/op) and 0 allocations per operation (allocs/op).
+BenchmarkFactorialRecursive-12: This benchmark ran 81732176 iterations of the factorialRecursive
+ function. It took an average of 14.43 nanoseconds per operation, and there were 0 memory 
+ allocations (B/op) and 0 allocations per operation (allocs/op).
+Both benchmarks passed (PASS), indicating that the functions performed as expected.
+This output provides insights into the performance of both iterative and recursive approaches to
+ calculating factorials. In this case, the iterative approach (factorialIterative) appears to be faster 
+ than the recursive approach (factorialRecursive).
+*/
